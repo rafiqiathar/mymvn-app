c8690425a03577d1c2283cd60d63a8608a31f8ab
athar <athar.rafiqi@gmail.com>
2015-02-25 02:59:44 +0530

    added 1 integer

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World of Maven!" );
+        int x=0;
+    	System.out.println( "Hello World of Maven!" + x );
+        
     }
 }
